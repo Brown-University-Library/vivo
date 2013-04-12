b3730831e78219b7dd7a849b729d12fe4f8fce3c
Ted Lawless <lawlesst@gmail.com>
2013-04-12 10:11:27 -0400

    Adding filter and Solr config/schema changes.

---
+++ productMods/WEB-INF/pyfilter/EtagFilter.py
@@ -0,0 +1,90 @@
+"""
+Servlet filter to generate Etags for VIVO.
+"""
+
+from java.io import File
+from java.io import FileInputStream
+from java.util import Properties
+from javax.servlet import Filter
+from javax.servlet.http import HttpServletResponse
+
+#Python
+import os
+import json
+import urllib
+from urlparse import urlparse
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+#Read VIVO deploy.properties file.
+webinf = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+properties_file = os.path.join(webinf, 'classes/deploy.properties')
+in_stream = FileInputStream(properties_file)
+properties = Properties()
+properties.load(in_stream) 
+
+VIVO_SOLR = properties.getProperty('vitro.local.solr.url').strip()
+VIVO_NS = properties.getProperty('Vitro.defaultNamespace').strip()
+ETAG_FIELD = 'etag'
+
+class EtagFilter(Filter):
+    
+    def get_solr_doc(self, vivo_id):
+        query = 'select/?q=URI:%(ns)s%(id)s&wt=json&fl=%(fields)s'
+        params = query % {'ns': VIVO_NS,
+                'id': vivo_id,
+                'fields': ETAG_FIELD}
+        solr_query_url = VIVO_SOLR + '/' + params
+        logging.debug("Solr URL: " + solr_query_url)
+        solr_handle = urllib.urlopen(solr_query_url)
+        solr_resp = json.load(solr_handle)
+        try:
+            solr_doc = solr_resp['response']['docs'][0]
+        except IndexError:
+            logging.debug("No Solr document for for %s." % vivo_id)
+            return
+        return solr_doc
+
+
+    def get_etag(self, solr_doc):
+        if solr_doc is None:
+            logging.debug("No Solr document found.")
+            return
+        etag = solr_doc.get(ETAG_FIELD)
+	if etag is None:
+	    logging.debug("No ETag found in Solr doc.")
+        return etag
+
+
+    def get_url_individual(self, url_string):
+        p = urlparse(url_string)
+        individual = p.path.split('/')[-1]
+        return individual
+
+    def doFilter(self, request, response, chain):
+        #Don't generate etags for logged in users.  
+        login_status = request.session.getValue('loginStatus')
+        if (login_status) and (login_status.isLoggedIn()):
+            logging.debug("User is logged in.  Caching disabled.")
+            #ToDo: figure out best way to do this with mod_cache
+            #Tell mod_cache to never cache this page
+            response.setHeader("Vary", "*")
+        else:
+            url_string = str(request.getRequestURL())
+            individual = self.get_url_individual(url_string)
+            doc = self.get_solr_doc(individual)
+            etag = self.get_etag(doc)
+            if etag is not None:
+                non_match = request.getHeader("If-None-Match")
+                #If we have an incoming matching etag return 304.
+                if (non_match) and (non_match == etag):
+                    logging.debug('Etag matched.') 
+                    return response.sendError(HttpServletResponse.SC_NOT_MODIFIED)
+                else:
+                    logging.debug('Etag did not match.')
+                    #Else set the new etag.
+                    response.setHeader("ETag", '%s' % etag)
+        #Always
+        chain.doFilter(request, response)
